14a1ccbda8c9286a8a4344329d9de7b8fccebac8
savelflow18 <2012formygames2012@gmail.com>
2025-04-21 22:37:14 +0300

    task10

---
+++ modul1/task10.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := [5]int{1, 2, 2, 5, 6}
+	var c []int
+	for i := 0; i < len(a); i++ {
+		b := 1
+		for j := 0; j < len(c); j++ {
+			if a[i] == c[j] {
+				b = 0
+			}
+		}
+		if b == 1 {
+			c = append(c, a[i])
+		}
+	}
+
+	fmt.Println(c)
+}
